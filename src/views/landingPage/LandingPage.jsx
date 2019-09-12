62c4a41fe795eb67b99045d9c4750fad48e57f7a
Caio Ragazzi <c.gemignani@inttecnologia.com.br>
2019-09-12 11:43:37 -0300

    first commit

---
+++ src/views/landingPage/LandingPage.jsx
@@ -0,0 +1,94 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import RGNavbar from "components/Navbars/NavBarRG.jsx";
+import Footer from "components/Footer/Footer.jsx";
+
+import bigChartData from "variables/charts.jsx";
+
+class LandingPage extends React.Component {
+  componentDidMount() {
+    document.body.classList.toggle("landing-page");
+  }
+  componentWillUnmount() {
+    document.body.classList.toggle("landing-page");
+  }
+  render() {
+    return (
+      <>
+        <RGNavbar />
+        <div className="wrapper">
+          <div className="page-header">
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/blob.png")}
+            />
+            <img
+              alt="..."
+              className="path2"
+              src={require("assets/img/path2.png")}
+            />
+            <img
+              alt="..."
+              className="shapes triangle"
+              src={require("assets/img/triunghiuri.png")}
+            />
+            <img
+              alt="..."
+              className="shapes wave"
+              src={require("assets/img/waves.png")}
+            />
+            <img
+              alt="..."
+              className="shapes squares"
+              src={require("assets/img/patrat.png")}
+            />
+            <img
+              alt="..."
+              className="shapes circle"
+              src={require("assets/img/cercuri.png")}
+            />
+          </div>
+          {/* INCLUIR SECTIONS!!!!!!!!!!!!!! */}          
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default LandingPage;
